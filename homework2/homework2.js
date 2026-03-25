b423f80aa0346e43d7a079d7e74547c433163b85
Donavonr <donavon.r25@gmail.com>
2026-03-24 23:50:19 -0500

    Update homework2.js

--- homework2/homework2.js
+++ homework2/homework2.js
@@ -2,13 +2,13 @@ document.getElementById("today").innerHTML =
     new Date().toLocaleDateString();
 function reviewForm() {
 
-    let firstName = document.getElementsByName("firstName")[0].value;
-    let lastName = document.getElementsByName("lastName")[0].value;
+    let firstname = document.getElementsByName("firstname")[0].value;
+    let lastname = document.getElementsByName("lastname")[0].value;
     let email = document.getElementsByName("email")[0].value;
 
     let output = `
         <h2>PLEASE REVIEW THIS INFORMATION</h2>
-        <p><strong>Name:</strong> ${firstName} ${lastName}</p>
+        <p><strong>Name:</strong> ${firstname} ${lastname}</p>
         <p><strong>Email:</strong> ${email}</p>
     `;
 
