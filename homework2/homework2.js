c180a12fa06c1ce8288d4fc0344faba6368c2c58
Donavonr <donavon.r25@gmail.com>
2026-03-26 01:28:55 -0500

    Update homework2.js

--- homework2/homework2.js
+++ homework2/homework2.js
@@ -120,7 +120,16 @@ if (dob === "") {
         ssnStatus = "ERROR: Invalid SSN";
         ssnColor = "red";
     }
+    // USER ID CHECK
+    let userIdStatus = "pass";
+    let userIdColor = "limegreen";
 
+    let userIdPattern = /^[a-zA-Z][a-zA-Z0-9_-]{4,29}$/;
+    
+    if (!userIdPattern.test(userId)) {
+        userIdStatus = "ERROR: Must start with a letter, 5–30 chars, no spaces";
+        userIdColor = "red";
+    }
     // Password check
    let passwordStatus = "pass";
 let passwordColor = "limegreen";
