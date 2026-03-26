a28b83c9891bb043327b24041f4c165e19a2120a
Donavonr <donavon.r25@gmail.com>
2026-03-25 23:43:48 -0500

    Update homework2.js

--- homework2/homework2.js
+++ homework2/homework2.js
@@ -38,6 +38,8 @@ function reviewForm() {
         conditionsList.push("None selected");
     }
 
+    let addressLine2 = addr2 ? `<p><strong>Address Line 2:</strong> ${addr2}</p>` : "";
+
     let output = `
     <h2>PLEASE REVIEW THIS INFORMATION</h2>
 
@@ -72,19 +74,23 @@ function checkPasswords() {
     let pw1 = document.getElementById("password").value;
     let pw2 = document.getElementById("password2").value;
     let msg = document.getElementById("passwordError");
+    let pw2Field = document.getElementById("password2");
 
     if (pw2 === "") {
         msg.innerHTML = "";
+        pw2Field.setCustomValidity("");
         return true;
     }
 
     if (pw1 !== pw2) {
         msg.innerHTML = "ERROR: Passwords do not match";
         msg.style.color = "red";
+        pw2Field.setCustomValidity("Passwords do not match");
         return false;
     } else {
         msg.innerHTML = "Passwords match";
         msg.style.color = "green";
+        pw2Field.setCustomValidity("");
         return true;
     }
 }
